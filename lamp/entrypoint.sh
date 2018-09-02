7770377694b0d39c81a1b5ebd50aa75b3148f9e9
purerosefallen <78877@qq.com>
2018-09-02 18:10:41 +0800

    add new

---
+++ lamp/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -n "$authorized_keys" ] && [ ! -f /root/.ssh/authorized_keys ]; then mkdir /root/.ssh;  printenv authorized_keys > /root/.ssh/authorized_keys; chmod 600 /root/.ssh/authorized_keys; fi
+if [ -n "$password" ] && passwd --status | grep -q 'L'; then echo "root:$password" | chpasswd ; fi
+unset authorized_keys
+unset password
+
+VOLUME_HOME="/var/lib/mysql"
+
+sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
+if [[ ! -d $VOLUME_HOME/mysql ]]; then
+    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
+    echo "=> Installing MySQL ..."
+    mysql_install_db > /dev/null 2>&1
+    echo "=> Done!"  
+    /create_mysql_admin_user.sh
+else
+    echo "=> Using an existing volume of MySQL"
+fi
+
+/usr/sbin/sshd -D
